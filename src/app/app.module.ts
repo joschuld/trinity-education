ecd0263cafe2b4eac27412443f6798e227222a8e
joschuld <91635911+joschuld@users.noreply.github.com>
2023-06-21 15:59:27 -0500

    Added base components needed for webstite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ApplyComponent } from './apply/apply.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ApplyComponent
   ],
   imports: [
     BrowserModule
