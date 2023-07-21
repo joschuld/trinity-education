bda3cc6475cc0f6b87ddd4e7f24b0fbdef316a8e
joschuld <91635911+joschuld@users.noreply.github.com>
2023-07-21 10:37:02 -0500

    testing prod

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -11,5 +12,38 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  teacherList = [
+
+    {
+      "name": "John Doe",
+      "education": "Master's Degree in Education",
+      "subject": "Mathematics",
+      "experience": "10 years of teaching experience",
+      "achievements": "Recipient of the Best Math Teacher Award"
+      },
+      {
+      "name": "Jane Smith",
+      "education": "Bachelor's Degree in English Literature",
+      "subject": "English Language and Literature",
+      "experience": "8 years of teaching experience",
+      "achievements": "Published author of several short stories"
+      },
+      {
+      "name": "Michael Johnson",
+      "education": "Master's Degree in Science",
+      "subject": "Physics",
+      "experience": "12 years of teaching experience",
+      "achievements": "Contributed to research in quantum physics"
+      },
+      {
+      "name": "Emily Thompson",
+      "education": "Ph.D. in History",
+      "subject": "Social Studies",
+      "experience": "15 years of teaching experience",
+      "achievements": "Presented papers at international history conferences"
+      }
+  
+  ]
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'trinity-education';
 }
+
+
+const teacherList: any[] = [
+
+  {
+    "name": "John Doe",
+    "education": "Master's Degree in Education",
+    "subject": "Mathematics",
+    "experience": "10 years of teaching experience",
+    "achievements": "Recipient of the Best Math Teacher Award"
+    },
+    {
+    "name": "Jane Smith",
+    "education": "Bachelor's Degree in English Literature",
+    "subject": "English Language and Literature",
+    "experience": "8 years of teaching experience",
+    "achievements": "Published author of several short stories"
+    },
+    {
+    "name": "Michael Johnson",
+    "education": "Master's Degree in Science",
+    "subject": "Physics",
+    "experience": "12 years of teaching experience",
+    "achievements": "Contributed to research in quantum physics"
+    },
+    {
+    "name": "Emily Thompson",
+    "education": "Ph.D. in History",
+    "subject": "Social Studies",
+    "experience": "15 years of teaching experience",
+    "achievements": "Presented papers at international history conferences"
+    }
+
+]
